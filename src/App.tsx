2c9b542544c176f0333962cda4b27fc18157b96f
Obie <obiemunozjr@gmail.com>
2023-01-14 23:15:36 -0500

    Updated text

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import FadingText from "./FadingText";
 
 function App() {
   const text: string =
-    "Packaging, websites, microsites, fulfillment integration, brand, catalogs, shipping boxes, direct mail, email campaigns, copywriting, photoshoots, art direction, delivery truck design, whatever it takes to help the Cowgirls sell their cheese.";
+    "Object-oriented programming, functional programming, data structures, algorithms, design patterns, machine learning, artificial intelligence, cloud computing, devops, software testing, agile methodologies, software architecture, code refactoring, whatever it takes to build efficient and scalable software.";
 
   return (
     <div className="app">
